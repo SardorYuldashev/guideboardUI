d6d07d49e20f0900d03de008653bc527b44d0438
Sardor <henkook-baron@mail.ru>
2023-09-09 21:45:45 +0500

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { ToastContainer } from 'react-toastify'
+import { ToastContainer } from 'react-toastify';
 
 // Axios
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000';
-// axios.defaults.baseURL = 'https://guideboard.up.railway.app/';
+axios.defaults.baseURL = 'https://guideboard.up.railway.app';
+// axios.defaults.baseURL = 'http://localhost:3000';
 axios.defaults.headers.common["Content-Type"] = "application/json";
 let token = localStorage.getItem("token");
 if (token) axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
